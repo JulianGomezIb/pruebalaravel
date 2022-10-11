cf7705d2454518e875090cd76b1779125dc92c2f
JulianGomezIb <julian.gomez@inglesparatodos.edu.co>
2022-10-11 09:21:23 -0500

    update node

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environments } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  baseUrl=environments.baseUrl;
+  baseUrl=environment.baseUrl;
   private SERVICE_URL = 'https://servicepokemon.000webhostapp.com/api'
 
   httpOptions: any;
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environments } from './environments/environments';
+import { environment } from './environments/environment';
 
-if (environments.production) {
+if (environment.production) {
   enableProdMode();
 }
 
